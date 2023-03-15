03e5aa074735c60a6c55e66f6cbbb06422c44971
subin <subinshrestha429@gmail.com>
2023-03-15 15:00:22 +0545

    feat: rspec setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -67,4 +69,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # rails generator config
+  config.generators do |g|
+    g.test_framework :rspec
+    g.fixture_replacement :factory_bot, {dir: "spec/factories"}
+  end
 end
